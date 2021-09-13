05f61622b01854ebd270cea69cbfcf19b8ac9b30
Lucile THIENOT <l.thienot@groupeonepoint.com>
2021-09-13 13:26:19 +0000

    Reordonnancement enrichi

--- src/main/java/com/acme/NamedLambdaPrinter.java
+++ src/main/java/com/acme/NamedLambdaPrinter.java
@@ -3,13 +3,22 @@ package com.acme;
 import java.util.List;
 import java.util.function.Consumer;
 
-// tag::named-lambda[]
+
 public class NamedLambdaPrinter {
     public void printElements(List<String> myList, List<String> theirList) {
-        Consumer<String> elementPrinter = element -> System.out.println("This is one element of my list: " + element);
+        // tag::before-named-lambda2[]
+        // tag::before-named-lambda1[]
+myList.forEach(element -> System.out.println("This is one element of my list: " + element));
+        // end::before-named-lambda1[]
+theirList.forEach(element -> System.out.println("This is one element of my list: " + element));
+        // end::before-named-lambda2[]
 
-        myList.forEach(elementPrinter);
-        theirList.forEach(elementPrinter);
+        // tag::named-lambda[]
+Consumer<String> elementPrinter = element -> System.out.println("This is one element of my list: " + element);
+
+myList.forEach(elementPrinter);
+theirList.forEach(elementPrinter);
+        // end::named-lambda[]
     }
 }
-// end::named-lambda[]
+
